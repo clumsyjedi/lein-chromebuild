e7df2c14fd4a96a433a038c9ce36806adbe159d7
Fraz <frazer.irving@mailonline.co.uk>
2014-11-26 16:13:34 +1100

    touch up project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject lein-chromebuild "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin for building Chrome browser extensions"
+  :url "https://github.com/clumsyjedi/lein-chromebuild"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :dependencies [[lein-cljsbuild "1.0.1"]
                  [cljsbuild "1.0.3"]
-                 [clojure-watch "0.1.9"]]
-  )
+                 [clojure-watch "0.1.9"]])
